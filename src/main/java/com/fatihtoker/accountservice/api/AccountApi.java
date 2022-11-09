ff8f7d0dce7f763d37036cf0f6b0cefce93dd79a
fatih <fatih16@live.nl>
2022-11-09 12:14:23 +0300

    Initial commit

---
+++ src/main/java/com/fatihtoker/accountservice/api/AccountApi.java
@@ -0,0 +1,43 @@
+package com.fatihtoker.accountservice.api;
+
+import com.fatihtoker.accountservice.entity.Account;
+import com.fatihtoker.accountservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("account")
+/**
+ * localhost:8080/account
+ * GET PUT DELETE POST
+ */
+public class AccountApi {
+
+    private final AccountService accountService;
+
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> get(@PathVariable("id") String id){
+       return ResponseEntity.ok(accountService.get(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> save(@RequestBody Account account){
+        return ResponseEntity.ok(accountService.save(account));
+    }
+    @PutMapping
+    public ResponseEntity<Account> update(Account account){
+        return ResponseEntity.ok(accountService.update(account));
+    }
+
+    @DeleteMapping
+    public void delete(String id){
+        accountService.delete(id);
+    }
+
+}
---
+++ src/main/java/com/fatihtoker/accountservice/service/AccountService.java
@@ -0,0 +1,24 @@
+package com.fatihtoker.accountservice.service;
+
+import com.fatihtoker.accountservice.entity.Account;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public Account get(String id){
+        return  new Account("test-id");
+
+    }
+    public Account save(Account account){
+        return  new Account("test-id");
+    }
+    public Account update(Account account){
+        return  new Account("test-id");
+    }
+    public void delete(String id){
+
+    }
+
+}
